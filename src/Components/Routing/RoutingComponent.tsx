a71398d2161c67ec6a84302e5847bed96dd84861
usamamukarram <usamamukarram217@gmail.com>
2026-06-03 17:04:51 +0500

    refactor: remove unused React imports from EmptyCart, Dashboard, and Thankyou components

--- src/Components/Routing/RoutingComponent.tsx
+++ src/Components/Routing/RoutingComponent.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
-import BookDetail from "../BookDetail/BookDetail";
+import BookDetail from "../BookDetail/Bookdetail";
 import Dashboard from "../Dashboard/Dashboard";
 import NotFound from "../NotFound/NotFound";
 import Products from "../Products/Products";
